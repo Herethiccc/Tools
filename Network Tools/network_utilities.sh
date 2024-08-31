99b67ac305c533e3f872a485b8cc3c7d543f2429
Herethicc <100536094+Herethiccc@users.noreply.github.com>
2024-08-31 23:30:17 +0200

    Update network_utilities.sh

--- Network Tools/network_utilities.sh
+++ Network Tools/network_utilities.sh
@@ -1,7 +1,45 @@
 #!/bin/bash
 
+#######################################################
+#######            Network Utilities            #######
+#######################################################
+####### Version release date: August 31st 2024  #######
+#######################################################
+
 #TODO
-#CREATE MENU
-#CREATE NETCAT FUNCTION
+#ADD MORE FUNCTIONS
 #TEST
 #ADD SOME WEIRD FANCY STUFF CAUSE YOU KNOW
+
+# Clear screen
+clear
+
+# Display menu
+echo "NETWORK UTILITIES"
+echo "1. Ping"
+echo "2. Netcat"
+echo "3. Traceroute"
+echo ""
+
+#Read user input and save it as $choice variable
+read -p "Pick an option:" choice
+
+# Launch command depending on the value stored in $choice
+case $choice in
+    1|ping|Ping|p|P)
+        read -p "Target ?" target
+        ping $target
+        ;;
+    2|netcat|Netcat|n|N)
+        read -p "Target ?" target
+        read -p "Port ?" port
+        nc -vz $target $port
+        ;;
+    3|traceroute|Traceroute|t|T)
+        read -p "Target ?" target
+        traceroute $target
+        ;;
+    *)
+        echo "Invalid option selected."
+        ;;
+esac
