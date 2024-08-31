68e6f954a5f01820ab61d0ffec15df168af494ad
Herethicc <100536094+Herethiccc@users.noreply.github.com>
2024-09-01 00:44:03 +0200

    Update system_utilities.sh

--- System Tools/system_utilities.sh
+++ System Tools/system_utilities.sh
@@ -25,6 +25,7 @@ while true; do
     echo "${yellow}/!\\ Please make sure to run this script with privileges /!\\${reset}"
     echo "SYSTEM UTILITIES"
     echo "1. List local users"
+    echo "2. List a user's group"
     echo "Q. Quit"
     echo ""
 
@@ -37,6 +38,11 @@ while true; do
             echo -e "${green}Listing local users:${reset}"
             sudo cat /etc/passwd
             ;;
+        2.)
+            read -p "User ?" user
+            echo -e "${green}User $user is part of the following groups:${reset}"
+            sudo groups $user
+        ;;
         Q|quit|Quit|q)
             echo -e "Goodbye!"
             break
