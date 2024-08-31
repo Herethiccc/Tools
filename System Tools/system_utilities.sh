986b9d06a5dde3b75a1eceea4a6e2e548e018830
Herethicc <100536094+Herethiccc@users.noreply.github.com>
2024-09-01 00:16:31 +0200

    Create system_utilities.sh

---
+++ System Tools/system_utilities.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#######################################################
+#######             System Utilities            #######
+#######################################################
+####### Version release date: August 31st 2024  #######
+#######################################################
+
+#TODO
+#ADD MORE FUNCTIONS
+#TEST
+#ADD SOME WEIRD FANCY STUFF CAUSE YOU KNOW
+
+reset='\e[0m'
+green='\e[32m'
+red='\e[31m'
+yellow='\e[1;33m'
+
+# Clear screen
+clear
+
+# Display menu
+while true; do
+
+    echo "${yellow}/!\\ Please make sure to run this script with privileges /!\\${reset}"
+    echo "SYSTEM UTILITIES"
+    echo "1. List local users"
+    echo "Q. Quit"
+    echo ""
+
+    #Read user input and save it as $choice variable
+    read -p "Pick an option:" choice
+
+    # Launch command depending on the value stored in $choice
+    case $choice in
+        1)
+            echo -e "${green}Listing local users:${reset}"
+            sudo cat /etc/passwd
+            ;;
+        Q|quit|Quit|q)
+            echo -e "Goodbye!"
+            break
+            ;; 
+        *)
+            echo -e "${red}Invalid option${reset}."
+            ;;
+    esac
+done
