42261573a8fbe90cedb769eb23b163dbd6f80825
Herethicc <100536094+Herethiccc@users.noreply.github.com>
2024-09-01 20:10:31 +0200

    Update network_utilities.sh

--- Network Tools/network_utilities.sh
+++ Network Tools/network_utilities.sh
@@ -23,6 +23,7 @@ clear
 while true; do
 
     echo "NETWORK UTILITIES"
+    echo "${yellow}/!\\ Please make sure to run this script with privileges /!\\${reset}"
     echo "1. Ping"
     echo "2. Netcat"
     echo "3. Traceroute"
@@ -36,27 +37,39 @@ while true; do
     # Launch command depending on the value stored in $choice    
     case $choice in
         1|ping|Ping|p|P)
+            clear
             read -p "Target? " target
             ping $target
             ;;
         2|netcat|Netcat|n|N)
+            clear
             read -p "Target? " target
             read -p "Port? " port
             nc -vz $target $port
             ;;
         3|traceroute|Traceroute|t|T)
+            clear
             read -p "Target? " target
             traceroute $target
             ;;
-        4|tcpdump|TCPDUMP|tcp|TCP
-            read -p "Source host ?" source
-            sudo tcpdump host $source
+        4|tcpdump|TCPDUMP|tcp|TCP)
+            clear
+
+            # Check if tcpdump is installed before running it
+            if command -v tcpdump &> /dev/null; then
+                read -p "Source host ?" source
+                sudo tcpdump host $source
+            else
+                echo -e "${red}TCPDUMP is NOT installed!${reset}"
+            fi
             ;;
         Q|quit|Quit|q)
+            clear
             echo -e "Goodbye!"
             break
             ;;
         *)
+            clear
             echo -e "${red}Invalid option${reset}."
             ;;
     esac
